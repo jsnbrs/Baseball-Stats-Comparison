177467b668da545a71d2ec144f684be22eaf71a0
Jason Booras <jasonbooras@gmail.com>
2016-03-03 12:24:55 -0500

    previous json numbers now clearing on next search

--- mlb/app/assets/javascripts/application.js
+++ mlb/app/assets/javascripts/application.js
@@ -38,79 +38,80 @@ $(function(){
     url: "/players/" + age +"/" + player1 + "/" + player2,
     dataType: "JSON",
       success: function(data){
+        $('.json').empty();
 
         var player_1_div1 = $(`
-          <div>${data[0].pa}</div>
+          <div class="json">${data[0].pa}</div>
           `).appendTo( '#pa1' );
         var player_1_div2 = $(`
-          <div>${data[0].avg}</div>
+          <div class="json">${data[0].avg}</div>
           `).appendTo( '#avg1' );
         var player_1_div3 = $(`
-          <div>${data[0].r}</div>
+          <div class="json">${data[0].r}</div>
           `).appendTo( '#r1' );
         var player_1_div4 = $(`
-          <div>${data[0].hr}</div>
+          <div class="json">${data[0].hr}</div>
           `).appendTo( '#hr1' );
         var player_1_div5 = $(`
-          <div>${data[0].bb}</div>
+          <div class="json">${data[0].bb}</div>
           `).appendTo( '#bb1' );
         var player_1_div6 = $(`
-          <div>${data[0].so}</div>
+          <div class="json">${data[0].so}</div>
           `).appendTo( '#so1' );
         var player_1_div7 = $(`
-          <div>${data[0].rbi}</div>
+          <div class="json">${data[0].rbi}</div>
           `).appendTo( '#rbi1' );
         var player_1_div8 = $(`
-          <div>${data[0].sb}</div>
+          <div class="json">${data[0].sb}</div>
           `).appendTo( '#sb1' );
         var player_1_div9 = $(`
-          <div>${data[0].obp}</div>
+          <div class="json">${data[0].obp}</div>
           `).appendTo( '#obp1' );
         var player_1_div10 = $(`
-          <div>${data[0].slg}</div>
+          <div class="json">${data[0].slg}</div>
           `).appendTo( '#slg1' );
         var player_1_div11 = $(`
-          <div>${data[0].ops}</div>
+          <div class="json">${data[0].ops}</div>
           `).appendTo( '#ops1' );
         var player_1_div12 = $(`
-          <div>${data[0].iso}</div>
+          <div class="json">${data[0].iso}</div>
           `).appendTo( '#iso1' );
 
         var player_2_div1 = $(`
-          <div>${data[1].pa}</div>
+          <div class="json">${data[1].pa}</div>
           `).appendTo( '#pa2' );
         var player_2_div2 = $(`
-          <div>${data[1].avg}</div>
+          <div class="json">${data[1].avg}</div>
           `).appendTo( '#avg2' );
         var player_2_div3 = $(`
-          <div>${data[1].r}</div>
+          <div class="json">${data[1].r}</div>
           `).appendTo( '#r2' );
         var player_2_div4 = $(`
-          <div>${data[1].hr}</div>
+          <div class="json">${data[1].hr}</div>
           `).appendTo( '#hr2' );
         var player_2_div5 = $(`
-          <div>${data[1].bb}</div>
+          <div class="json">${data[1].bb}</div>
           `).appendTo( '#bb2' );
         var player_2_div6 = $(`
-          <div>${data[1].so}</div>
+          <div class="json">${data[1].so}</div>
           `).appendTo( '#so2' );
         var player_2_div7 = $(`
-          <div>${data[1].rbi}</div>
+          <div class="json">${data[1].rbi}</div>
           `).appendTo( '#rbi2' );
         var player_2_div8 = $(`
-          <div>${data[1].sb}</div>
+          <div class="json">${data[1].sb}</div>
           `).appendTo( '#sb2' );
         var player_2_div9 = $(`
-          <div>${data[1].obp}</div>
+          <div class="json">${data[1].obp}</div>
           `).appendTo( '#obp2' );
         var player_2_div10 = $(`
-          <div>${data[1].slg}</div>
+          <div class="json">${data[1].slg}</div>
           `).appendTo( '#slg2' );
         var player_2_div11 = $(`
-          <div>${data[1].ops}</div>
+          <div class="json">${data[1].ops}</div>
           `).appendTo( '#ops2' );
         var player_2_div12 = $(`
-          <div>${data[1].iso}</div>
+          <div class="json">${data[1].iso}</div>
           `).appendTo( '#iso2' );
 
 
