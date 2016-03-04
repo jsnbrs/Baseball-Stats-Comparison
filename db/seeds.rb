4d62d15192302d70bc30edfc310f77be2f0b0c35
Jason Booras <jasonbooras@gmail.com>
2016-03-03 22:01:17 -0500

    seeds file issue with copy

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 ActiveRecord::Base.connection.execute(<<-SQL)
 
-  COPY players
+  \COPY players
     (PLAYER_ID, NAME, YEAR, AGE, G, PA, AB, R, H, single, double, triple, HR, TB, BB, IBB, SO, HBP, SF, SH, RBI, OBI, DP, NETDP, SB, CS, SBP, AVG, OBP, SLG, OPS, ISO, BPF, oppOPS, TAv, VORP, FRAA, BWARP, extra)
   FROM '#{Rails.root.join('db/players.csv')}'
     WITH  DELIMITER ',' CSV HEADER;
